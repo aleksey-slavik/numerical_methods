a06f469e03b5622d497b9077f6aa5504ca75ae88
oleksii.slavik <oleksii.slavik@globallogic.com>
2024-09-03 14:30:15 +0300

    Add connection test

--- ua/nure/am/numerical_methods/tests/lesson1.py
+++ ua/nure/am/numerical_methods/tests/lesson1.py
@@ -1,8 +1,17 @@
+def print_test_result(condition):
+    if condition:
+        print("Test passed.")
+    else:
+        print("Test not passed. Try again.")
+
+def test_connection():
+    print("Test passed.")
+
+
 def task1(variant, number, significant_numbers):
     answers = {
         0: [0.001230, "1, 2, 3, 0"],
         1: []
     }
 
-    return "Test passed." if (answers.get(variant)[0] == number and
-                              answers.get(variant)[1] == significant_numbers) else "Test not passed. Try again."
+    print_test_result(answers.get(variant)[0] == number and answers.get(variant)[1] == significant_numbers)
